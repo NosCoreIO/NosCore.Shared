a5063a28433db831ec9ee1a00705e937b85948db
erwan-joly <erwan.joly@pushpay.com>
2020-05-21 20:53:33 +1200

    add reput and dignity

---
+++ src/NosCore.Shared/Enumerations/DignityType.cs
@@ -0,0 +1,18 @@
+﻿//  __  _  __    __   ___ __  ___ ___
+// |  \| |/__\ /' _/ / _//__\| _ \ __|
+// | | ' | \/ |`._`.| \_| \/ | v / _|
+// |_|\__|\__/ |___/ \__/\__/|_|_\___|
+// -----------------------------------
+
+namespace NosCore.Shared.Enumerations
+{
+    public enum ReputationType : byte
+    {
+		Default = 1
+        Dubious = 2,
+        Dreadful = 3,
+        Unqualified = 4,
+        Useless = 5,
+        Failed = 6,
+    }
+}
