b2e48cddec2678e2f09523040ffdae4fd6ca3351
Erwan JOLY <35202750+erwan-joly@users.noreply.github.com>
2020-05-21 21:10:35 +1200

    Update DignityType.cs

--- src/NosCore.Shared/Enumerations/DignityType.cs
+++ src/NosCore.Shared/Enumerations/DignityType.cs
@@ -8,11 +8,11 @@ namespace NosCore.Shared.Enumerations
 {
     public enum ReputationType : byte
     {
-        Default = 1
+        Default = 1,
         Dubious = 2,
         Dreadful = 3,
         Unqualified = 4,
         Useless = 5,
-        Failed = 6,
+        Failed = 6
     }
 }
